69b60793d3a43f311eaace75719bacb2e9cbe9b8
Sophie LAURENT <sophielaurent92@gmail.com>
2024-12-17 16:17:39 +0100

    data.sql script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO USER (id, firstName, lastName, email, password, address, dateOfBirth, dateOfCreation)
+VALUES (1, "service", "clients", "email@serviceclient.com", "passwordadmin", "addresscie", "1984-07-14", "2024-12-13"),
+(2, "user", "client", "email@client.com", "passwordclient", "addressclient", "1989-12-14", "2024-12-13");
